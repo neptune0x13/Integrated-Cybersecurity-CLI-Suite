9408b6c05ed667313cae62c82fcd59fdb2e9da56
Neptune <80949976+neptune1337@users.noreply.github.com>
2021-05-15 14:12:01 +0530

    Update Macchanger.py

--- Tools by Nep/Macchanger/Macchanger.py
+++ Tools by Nep/Macchanger/Macchanger.py
@@ -3,14 +3,13 @@
 import subprocess
 
 a_file = open("nametag.txt")
-
 lines = a_file.readlines()
 for line in lines:
     print(line)
 a_file.close()
-interface = input("Enter interface:")
-new_mac =input("Enter MAC address")
-print("[+] Changing MAC address for:"+ interface +"to"+ new_mac)
+interface = input("Interface>")
+new_mac =input("MAC address>")
+print("[+] Changing MAC address for: "+ interface +" to "+ new_mac)
 subprocess.call("ifconfig " + interface + " down ", shell=True)
 subprocess.call("ifconfig " + interface + " hw ether "+new_mac, shell=True)
 subprocess.call("ifconfig " + interface + " up ", shell=True)
