a16f85dc09a47be3f131c3cbfab807af12a5839a
Neptune <80949976+neptune1337@users.noreply.github.com>
2021-05-15 13:39:00 +0530

    Update Macchanger.py

--- Tools by Nep/Macchanger/Macchanger.py
+++ Tools by Nep/Macchanger/Macchanger.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 import subprocess
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+
+a_file = open("nametag.txt")
+
+lines = a_file.readlines()
+for line in lines:
+    print(line)
+a_file.close()
+interface = input("Enter interface:")
+new_mac =input("Enter MAC address")
+print("[+] Changing MAC address for:"+ interface +"to"+ new_mac)
+subprocess.call("ifconfig " + interface + " down ", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether "+new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up ", shell=True)
