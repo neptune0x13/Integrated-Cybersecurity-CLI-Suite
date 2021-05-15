795657dd99da5fc645de772936bb0f28a562ee38
Neptune <80949976+neptune1337@users.noreply.github.com>
2021-05-15 15:33:24 +0530

    Update Macchanger.py

--- Tools by Nep/Macchanger/Macchanger.py
+++ Tools by Nep/Macchanger/Macchanger.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
-
 import subprocess
+import optparse
 
 a_file = open("nametag.txt")
 lines = a_file.readlines()
 for line in lines:
     print(line,end='╯')
 a_file.close()
-interface =input("Interface>")
-new_mac =input("MAC address>")
-print("[+] Changing MAC address for: "+ interface +" to "+ new_mac)
-subprocess.call(["ifconfig", interface,"down"])
-subprocess.call(["ifconfig", interface,"hw","ether",new_mac])
-subprocess.call(["ifconfig", interface,"up"])
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface",dest="interface", help="Interface to change the MAC address")
+    parser.add_option("-m","--mac",dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+          parser.error("[-] Please specify an interface, use --help for more info ")
+    elif not options.new_mac:
+          parser.error("[-] Please specify a new mac, use --help for more info ")
+    return options
+ 
+def change_mac(interface, new_mac):
+      print("[+] Changing MAC address for: "+ interface +" to "+ new_mac)
+      subprocess.call(["ifconfig", interface,"down"])
+      subprocess.call(["ifconfig", interface,"hw","ether",new_mac])
+      subprocess.call(["ifconfig", interface,"up"]) 
+      
+options = get_arguments()
+change_mac(options.interface,options.new_mac)
