3aecd84fea1b84ded33a99407f4c7f68e01160d3
Neptune <80949976+neptune1337@users.noreply.github.com>
2021-05-23 16:38:54 +0530

    Update Macchanger.py

--- Tools by Nep/Macchanger/Macchanger.py
+++ Tools by Nep/Macchanger/Macchanger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import subprocess
 import optparse
+import re
 
 subprocess.call(["figlet", "NEPTUNE"]) 
 
@@ -20,6 +21,28 @@ def change_mac(interface, new_mac):
       subprocess.call(["ifconfig", interface,"down"])
       subprocess.call(["ifconfig", interface,"hw","ether",new_mac])
       subprocess.call(["ifconfig", interface,"up"]) 
-      
+
+def get_input(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_result:
+        return mac_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
 options = get_arguments()
+
+current_mac = get_input(options.interface)
+print("Current MAC = " + str(current_mac))
+
 change_mac(options.interface,options.new_mac)
+
+current_mac = get_input(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to "+ current_mac)
+else:
+    print("[-] MAC address did not get changed.")
+
+
+
