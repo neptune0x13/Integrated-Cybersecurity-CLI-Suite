58bcb0b7f49e552a30fc87801527279473b6a5f8
Rhythin Raj <80949976+neptune0x13@users.noreply.github.com>
2023-03-25 10:36:34 +0530

    Update networkscanner.py

--- Tools by Nep/Network Scanner/networkscanner.py
+++ Tools by Nep/Network Scanner/networkscanner.py
@@ -8,15 +8,15 @@ print("\t\t\t\t\t\t by neptune0x13")
 
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-t","--target",dest="target",help="Target IP / IP range")   //Adding options
-    (options ,arguments) = parser.parse_args()                                     //Making the option as an command line argument 
+    parser.add_option("-t","--target",dest="target",help="Target IP / IP range")   #Adding options
+    (options ,arguments) = parser.parse_args()                                     #Making the option as an command line argument 
     return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request                                  //Creates a new packet with the ARP request packet as the payload and the Ethernet broadcast packet as the header.
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]  //Extracts the list of packets that received a response from the srp() function's return value.
+    arp_request_broadcast = broadcast/arp_request                                  #Creates a new packet with the ARP request packet as the payload and the Ethernet broadcast packet as the header.
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]  #Extracts the list of packets that received a response from the srp() function's return value.
 
     clients_list=[]
     for element in answered_list:
@@ -30,6 +30,6 @@ def print_result(results_list):
     for client in results_list:
         print(client["ip"]+"\t\t"+client["mac"])
 
-options = get_arguments()                                                          //To obtain the command-line arguments 
-scan_result = scan(options.target)                                                 //To perform the ARP scan and return the list
-print_result(scan_result)                                                          //To display the IP and MAC of connected devices  
+options = get_arguments()                                                          #To obtain the command-line arguments 
+scan_result = scan(options.target)                                                 #To perform the ARP scan and return the list
+print_result(scan_result)                                                          #To display the IP and MAC of connected devices  
